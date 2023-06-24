2952b830208cbb0bad7c442b6bce4644f4c17c65
Zennou <zennou.yin@gmail.com>
2023-06-24 10:17:53 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import *
+import my_dividend_scraper as div
+
+app = Flask(__name__)
+
+
+@app.route('/dividend/<ticker>')
+def dividend_JSON(ticker):
+    return div.get_dividendJSON(ticker)
+
+
+if __name__ == '__main__':
+    app.run(port=8675)
---
+++ my_dividend_scraper.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+import json
+import timeit
+
+
+def get_dividendJSON(symbol):
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+
+    # GoogleBot User Agent
+    options.add_argument(
+        f"--user-agent=Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html")
+
+    driver = webdriver.Firefox(options)
+    driver.get("https://seekingalpha.com/symbol/" +
+               symbol+"/dividends/scorecard")
+    sleep(1)
+    price = driver.find_element(
+        By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/div[2]/span[1]').text.replace('$', "")
+    dividend = driver.find_element(
+        By.XPATH, '//*[@id="content"]/div[2]/div/div[3]/div/div/section[3]/div/div[2]/div/div[1]/div[2]').text.replace('$', "")
+    frequency = driver.find_element(
+        By.XPATH, '//*[@id="content"]/div[2]/div/div[3]/div/div/section[3]/div/div[2]/div/div[6]/div[2]').text
+    driver.quit()
+    return json.dumps({'Price': float(price), 'Dividend Amount': float(dividend), 'Dividend Frequency': frequency})
+
+
+"""
+start = timeit.default_timer()
+print(get_dividendJSON("PSEC"))
+stop = timeit.default_timer()
+print(stop-start)
+"""
